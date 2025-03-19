6101b148f8df7198bf1343e5dbc509e49462a4e4
wuryanto <penshopx@gmail.com>
2025-03-19 19:44:37 +0700

    Add simplified date-picker for go-live

---
+++ components/ui/date-picker.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import * as React from "react"
+import { Button } from "@/components/ui/button"
+
+export function DatePicker() {
+  return (
+    <Button disabled className="w-[280px] justify-start text-left">
+      Date picker coming soon
+    </Button>
+  )
+}
